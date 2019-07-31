b4d7e677fa61154d0d8097bfd4867ac63af156fa
alex4o <mrabonin@gmail.com>
2019-07-31 11:13:56 +0300

    Adds application to build config so the project can be run through gradle only and the version number is semver

--- gattaka-queen/src/main/scala/CustomIndividualActor.scala
+++ gattaka-queen/src/main/scala/CustomIndividualActor.scala
@@ -9,6 +9,8 @@ class CustomIndividualActor(genome: Genome) extends Individual(genome) {
 
   import context.dispatcher
 
+
+
   override def customReceive = {
     case Initialize(data) =>
       val values = genome.chromosomes.head.toGene.value.asInstanceOf[Map[String, Double]]
@@ -17,6 +19,9 @@ class CustomIndividualActor(genome: Genome) extends Individual(genome) {
       val temp1 = Math.sin(Math.sqrt(x * x + y * y))
       val temp2 = 1 + 0.001 * (x * x + y * y)
       val fitness = (0.5 + (temp1 * temp1 - 0.5) / (temp2 * temp2)).toDouble
+
+      println(genome)
+
       context.system.scheduler.scheduleOnce((scala.util.Random.nextInt(1000)) milliseconds) {
         dispatchFitness(fitness)
       }
